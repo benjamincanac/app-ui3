82a39957519868cc80a875e9b455298de86e4ee7
Benjamin Canac <canacb1@gmail.com>
2024-11-07 12:37:24 +0100

    up

--- app/app.vue
+++ app/app.vue
@@ -3,8 +3,3 @@
     <NuxtPage />
   </div>
 </template>
-
-<style>
-@import "tailwindcss";
-@import "@nuxt/ui";
-</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Hello World</h1>
+    <h1 class="text-primary-500 dark:text-primary-400">Hello World</h1>
 
     <UButton>Click me</UButton>
   </div>
