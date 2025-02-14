853c00e74e9d37562fb75ea4f3d18ab245438e99
Benjamin Canac <canacb1@gmail.com>
2025-02-14 15:54:53 +0100

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui-pro'],
+  modules: ['@nuxt/ui'],
   future: {
     compatibilityVersion: 4
   },
   compatibilityDate: '2024-04-03',
   devtools: {
     enabled: true
-  },
-  vite: {
-    optimizeDeps: {
-      include: ['color']
-    }
   },
   css: ['~/assets/css/index.css']
 })
